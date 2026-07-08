ddbb3f99a008dcd773a1477fa87c01ea3a0cbb6b
Naser636 <kabalain441@gmail.com>
2026-07-08 09:33:21 +0000

    Sprint-01 : Capability Registry

---
+++ src/core/capability-registry.ts
@@ -0,0 +1,10 @@
+import { Capability } from "@/contracts/capability";
+
+export const capabilities: Capability[] = [
+  {
+    id: "decision-engine",
+    name: "Decision Engine",
+    version: "1.0.0",
+    description: "Premier moteur de décision d'ODG",
+  },
+];
